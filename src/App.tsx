2323fa3713a3f4acc86275de7604e1ff0ad8e940
Austin <137583592+austinlongmn@users.noreply.github.com>
2024-10-06 14:16:49 -0500

    Customized

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Austin's React App - now with wings!
         </p>
         <a
           className="App-link"
