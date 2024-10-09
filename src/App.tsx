5b44069c6908a525e6b572e86fcfeb6f2df4c4fb
Austin <austin@austinlong.dev>
2024-10-08 19:58:41 -0500

    Formatted code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Welcome to Austin's new personal website!<br/>It is still under
-          development. Check back soon for updates!
-				</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This site will be built with React.
+          Welcome to Austin's new personal website!
+          <br />
+          It is still under development. Check back soon for updates!
         </p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+        <p>This site will be built with React.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
