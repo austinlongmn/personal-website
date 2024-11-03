eac38bc0d14ebd981645220cc06bab203b49377b
Austin <austin@austinlong.dev>
2024-11-03 16:49:34 -0600

    Switched to pnpm and added www redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    'output': 'export'
+    'output': 'export',
+    async redirects() {
+        return [
+            {
+                source: 'https://www.austinlong.dev/:path*',
+                destination: 'https://austinlong.dev/:path*',
+                permanent: true
+            }
+        ]
+    }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1 className="mb-2 text-4xl">austinlong.dev</h1>
       <h2>Christ-centered software development</h2>
+      <p>This website is currently under development.</p>
     </div>
   );
 }
