1ffc804422205173ad26dcd825a2bcadef8be1bb
Austin <austin@austinlong.dev>
2024-11-03 17:25:19 -0600

    Removed Next.js redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    'output': 'export',
-    async redirects() {
-        return [
-            {
-                source: 'https://www.austinlong.dev/:path*',
-                destination: 'https://austinlong.dev/:path*',
-                permanent: true
-            }
-        ]
-    }
+    'output': 'export'
 };
 
 export default nextConfig;
