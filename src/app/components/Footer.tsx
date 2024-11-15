2bd3643ab9d54111a18ccaa1f29c6566cc0baecd
Austin <austin@austinlong.dev>
2024-11-15 17:07:19 -0600

    Worked on Footer.tsx
    Also made ESLint only do warnings

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,31 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Link } from "@nextui-org/react";
+import Image from "next/image";
+import github_logo from "./images/github-mark/github-mark-white.svg";
 
-export default function Footer({ className }: {className?: string }) {
+export default function Footer({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <Card>
+      <Card className="p-4">
         <CardHeader>
           <h2>Contact</h2>
         </CardHeader>
-        <CardBody>
-          <p>GitHub</p>
+        <CardBody className="p-4">
+          <div className="flex flex-row gap-4">
+            <Link href="https://github.com/austinlongmn">
+              <Image
+                src={github_logo}
+                alt="GitHub Logo"
+                width={30}
+                height={30}
+                className="pr-2"
+              />
+              <p className="pl-2">GitHub</p>
+            </Link>
+            <p>Email:</p>
+            <Link href="mailto:austin@austinlong.dev">
+              austin@austinlong.dev
+            </Link>
+          </div>
         </CardBody>
       </Card>
     </div>
